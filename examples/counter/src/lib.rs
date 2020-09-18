84fdbdafc50910d4beb697a85b7707694fd1839e
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-09-18 23:14:34 +0900

    konmari

---
+++ examples/counter/src/lib.rs
@@ -0,0 +1,51 @@
+use futures::prelude::*;
+use siro::{vdom, Mailbox};
+use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+#[derive(Default)]
+struct Model {
+    count: i32,
+}
+
+enum Msg {
+    Increment,
+}
+
+fn update(model: &mut Model, msg: Msg) {
+    match msg {
+        Msg::Increment => model.count += 1,
+    }
+}
+
+fn view(model: &Model, mailbox: &Mailbox<Msg>) -> impl Into<vdom::Node> {
+    vdom::element("button") //
+        .listener(mailbox.on_click(|_| Msg::Increment))
+        .child(format!("{}", model.count))
+}
+
+#[wasm_bindgen(start)]
+pub async fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+
+    let ctx = siro::global_context().ok_or("cannot create global context")?;
+
+    let mountpoint = ctx.select("#app").ok_or("cannot find `#app` in document")?;
+    siro::util::remove_children(&mountpoint)?;
+
+    let mut app = ctx.mount(mountpoint.as_ref())?;
+    let (mailbox, mut incomings) = Mailbox::pair();
+
+    let mut model = Model { count: 0 };
+    app.render(&ctx, view(&model, &mailbox))?;
+
+    while let Some(msg) = incomings.next().await {
+        update(&mut model, msg);
+        app.render(&ctx, view(&model, &mailbox))?;
+    }
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,14 @@
-use futures::prelude::*;
-use siro::{vdom, Mailbox};
-use wasm_bindgen::prelude::*;
-use wee_alloc::WeeAlloc;
+#![feature(move_ref_pattern)] // Delete after https://github.com/rust-lang/rust/pull/76119 is merged
 
-#[global_allocator]
-static ALLOC: WeeAlloc = WeeAlloc::INIT;
+mod app;
+mod global;
+mod mailbox;
 
-#[derive(Default)]
-struct Model {
-    count: i32,
-}
+pub mod util;
+pub mod vdom;
 
-enum Msg {
-    Increment,
-}
-
-fn update(model: &mut Model, msg: Msg) {
-    match msg {
-        Msg::Increment => model.count += 1,
-    }
-}
-
-fn view(model: &Model, mailbox: &Mailbox<Msg>) -> impl Into<vdom::Node> {
-    vdom::element("button") //
-        .listener(mailbox.on_click(|_| Msg::Increment))
-        .child(format!("{}", model.count))
-}
-
-#[wasm_bindgen(start)]
-pub async fn main() -> Result<(), JsValue> {
-    console_error_panic_hook::set_once();
-
-    let ctx = siro::global_context().ok_or("cannot create global context")?;
-
-    let mountpoint = ctx.select("#app").ok_or("cannot find `#app` in document")?;
-    siro::util::remove_children(&mountpoint)?;
-
-    let mut app = ctx.mount(mountpoint.as_ref())?;
-    let (mailbox, mut incomings) = Mailbox::pair();
-
-    let mut model = Model { count: 0 };
-    app.render(&ctx, view(&model, &mailbox))?;
-
-    while let Some(msg) = incomings.next().await {
-        update(&mut model, msg);
-        app.render(&ctx, view(&model, &mailbox))?;
-    }
-
-    Ok(())
-}
+pub use crate::{
+    app::App, //
+    global::{global_context, GlobalContext},
+    mailbox::Mailbox,
+};
