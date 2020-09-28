920d58886a00b0fd21b6419718f8b70b5ead67f3
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-09-29 01:37:21 +0900

    improve xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,11 +1,42 @@
 use std::{
     env,
-    path::PathBuf,
+    path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 use walkdir::WalkDir;
 
 fn main() -> anyhow::Result<()> {
+    setup_path()?;
+
+    let examples_dir = project_root().join("examples");
+
+    if let Some(name) = env::args_os().nth(1) {
+        let manifest_dir = examples_dir.join(name);
+        build(&manifest_dir)?;
+        return Ok(());
+    }
+
+    let entries = WalkDir::new(&examples_dir)
+        .max_depth(2)
+        .into_iter()
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            if entry.file_name() == "Cargo.toml" {
+                Some(entry)
+            } else {
+                None
+            }
+        });
+
+    for entry in entries {
+        let manifest_dir = entry.path().parent().unwrap();
+        build(&manifest_dir)?;
+    }
+
+    Ok(())
+}
+
+fn setup_path() -> anyhow::Result<()> {
     if let Some(paths) = env::var_os("PATH") {
         let node_bins = project_root()
             .join("node_modules")
@@ -21,41 +52,29 @@ fn main() -> anyhow::Result<()> {
         env::set_var("PATH", &new_paths);
     }
 
-    let entries = WalkDir::new(project_root().join("examples"))
-        .max_depth(2)
-        .into_iter()
-        .filter_map(|entry| {
-            let entry = entry.ok()?;
-            if entry.file_name() == "Cargo.toml" {
-                Some(entry)
-            } else {
-                None
-            }
-        });
+    Ok(())
+}
 
-    for entry in entries {
-        let manifest_dir = entry.path().parent().unwrap();
+fn build(manifest_dir: &Path) -> anyhow::Result<()> {
+    let mut wasm_pack = Command::new("wasm-pack");
+    wasm_pack
+        .stdin(Stdio::null())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+    wasm_pack
+        .arg("build")
+        .arg("--dev")
+        .arg("--no-typescript")
+        .args(&["--target", "web"])
+        .args(&["--out-name", "index"]);
 
-        let mut wasm_pack = Command::new("wasm-pack");
-        wasm_pack
-            .stdin(Stdio::null())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit());
-        wasm_pack
-            .arg("build")
-            .arg("--dev")
-            .arg("--no-typescript")
-            .args(&["--target", "web"])
-            .args(&["--out-name", "index"]);
+    wasm_pack.arg("--out-dir").arg(manifest_dir.join("pkg"));
 
-        wasm_pack.arg("--out-dir").arg(manifest_dir.join("pkg"));
+    wasm_pack.arg(&manifest_dir);
 
-        wasm_pack.arg(&manifest_dir);
-
-        println!("[xtask] run command: {:?}", wasm_pack);
-        let status = wasm_pack.status()?;
-        anyhow::ensure!(status.success(), "wasm-pack failed with: {:?}", status);
-    }
+    println!("[xtask] run command: {:?}", wasm_pack);
+    let status = wasm_pack.status()?;
+    anyhow::ensure!(status.success(), "wasm-pack failed with: {:?}", status);
 
     Ok(())
 }
